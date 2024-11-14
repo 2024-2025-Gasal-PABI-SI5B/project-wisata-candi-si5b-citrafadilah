cd159a48befb351ef92e9e84a1f1dca73adee433
citrafadilah <citrafadilahhasibuan_2226240089@mhs.mdp.ac.id>
2024-11-14 07:58:49 +0700

    update progress

--- WISATA_CANDI/lib/main.dart
+++ WISATA_CANDI/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
-import 'package:wisata_candi/screens/detail.screen.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,11 +16,10 @@ class MainApp extends StatelessWidget {
       title: 'Wisata Candi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
+        useMaterial3: true,
       ),
-      home: DetailScreen(
-        candi: candiList[0]),
+      //home: DetailScreen(candi: candiList[0],),
+      home: ProfileScreen(),
     );
   }
 }
-
---
+++ WISATA_CANDI/lib/screens/signin_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Handle sign in logic here
+              },
+              child: Text('Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: SignInScreen(),
+  ));
+}
